79cf3b44eebe3c94484f969ff77df52cec6b1f25
Anton <antonsnizhko29@gmail.com>
2023-03-01 13:53:19 -0600

    wowowoow

--- navBar.php
+++ navBar.php
@@ -3,6 +3,6 @@
         <li class="column"><a href="index.php">Home</a></li>
         <li class="column"><a href="add_idea_form.php">Add</a></li>
         <li class="column"><a href="listView.php">Sort</a></li>
-        <li class="column"><a href="#">About Us</a></li>
+        <li class="column"><a href="about.php">About Us</a></li>
     </ul>
 </nav>
